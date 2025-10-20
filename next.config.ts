40680921c7208fcbaf5484543cbd7f0df76e3af2
sethwebster <sethwebster@gmail.com>
2025-10-20 12:57:41 -0400

    fix: Add Gravatar to allowed image hostnames
    - Added secure.gravatar.com to next.config.ts
    - Fixes GitLab user avatar display
    - GitLab uses Gravatar for profile pictures

--- next.config.ts
+++ next.config.ts
@@ -17,6 +17,10 @@ const nextConfig: NextConfig = {
         protocol: 'https',
         hostname: 'avatars.githubusercontent.com',
       },
+      {
+        protocol: 'https',
+        hostname: 'secure.gravatar.com',
+      },
     ],
   },
 };
