f08613ebad86178fd11403056bb989a94b5f77cc
sethwebster <sethwebster@gmail.com>
2026-06-15 23:20:16 -0400

    fix: add robots metadata route
    Signed-off-by: sethwebster <sethwebster@gmail.com>

---
+++ src/app/robots.test.ts
@@ -0,0 +1,16 @@
+import { describe, expect, it } from 'vitest';
+
+describe('robots metadata route', () => {
+  it('returns a crawl policy plus sitemap for the public site', async () => {
+    const { default: robots } = await import('./robots');
+
+    expect(robots()).toEqual({
+      rules: {
+        userAgent: '*',
+        allow: '/',
+      },
+      sitemap: 'https://react.foundation/sitemap.xml',
+      host: 'https://react.foundation',
+    });
+  });
+});
---
+++ src/app/robots.ts
@@ -0,0 +1,14 @@
+import { MetadataRoute } from 'next';
+
+const siteUrl = 'https://react.foundation';
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: '*',
+      allow: '/',
+    },
+    sitemap: `${siteUrl}/sitemap.xml`,
+    host: siteUrl,
+  };
+}
