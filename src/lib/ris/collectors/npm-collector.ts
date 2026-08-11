b087cc1b0e207bd80a120e3983b7e43e926b69ce
dougbot-agent <agent-doug@sethwebster.com>
2026-08-11 12:03:12 -0400

    Fix: add Base UI to supported ecosystem (closes #90) (#91)
    * Fix: add Base UI to supported ecosystem
    
    Signed-off-by: sethwebster <sethwebster@gmail.com>
    
    * Test: update tracked repository count
    
    Signed-off-by: sethwebster <sethwebster@gmail.com>
    
    ---------
    
    Signed-off-by: sethwebster <sethwebster@gmail.com>
    Co-authored-by: sethwebster <sethwebster@gmail.com>

--- src/lib/ris/collectors/npm-collector.ts
+++ src/lib/ris/collectors/npm-collector.ts
@@ -219,6 +219,7 @@ export class NPMCollector {
       'adobe/react-spectrum': '@adobe/react-spectrum',
       'ariakit/ariakit': '@ariakit/react',
       'mui/material-ui': '@mui/material',
+      'mui/base-ui': '@base-ui/react',
       'chakra-ui/chakra-ui': '@chakra-ui/react',
       'ant-design/ant-design': 'antd',
       'framer/motion': 'framer-motion',
